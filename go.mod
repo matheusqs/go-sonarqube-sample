374dd1bdce5850e8e1118663545cea2ac57b373a
Matheus Quintao <matheus.quintao.santiago@gmail.com>
2021-12-01 13:13:03 -0300

    Add sonar

---
+++ go.mod
@@ -0,0 +1,3 @@
+module sonarqube/go/sample
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world.")
+
+	if true {
+		fmt.Println("F")
+	}
+
+	if true {
+		fmt.Println("M")
+	}
+}
